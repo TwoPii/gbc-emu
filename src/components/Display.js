35df23ac2c75c1817fc7556305cfbdd3664df6be
Genís Bosch <genisbosch@gmail.com>
2023-07-28 16:07:49 +0200

    Display changes

--- src/components/Display.js
+++ src/components/Display.js
@@ -15,6 +15,9 @@ class Display {
         this.force_bg_loc = false;
         this.scanlines_rendered = 0;
         this.shades_of_gray = new Array(4);
+        this.bg_array = this.getFilledDisplay(255, 0, 255, 255);
+        this.window_array = this.getFilledDisplay(0, 0, 0, 0);
+        this.sprites_array = this.getFilledDisplay(0, 0, 0, 0);
 
         this.canvas = document.createElement('canvas');
         this.canvas.id ="display";
@@ -33,22 +36,45 @@ class Display {
         this.shades_of_gray[0x3] = "rgb(0, 0, 0)";
     }
 
-    getBgArray() {
-        ctx.rect(0,0,160, 144),
-        ctx.fillStyle= "rgb(255, 0, 255)";
-        ctx.fill();
+    getFilledDisplay(r, g, b, a) {
+        let array = new Uint8ClampedArray(160*144*4*this.scale);
+        for (let i = 0; i < array.length; i+= 4*this.scale) {
+            for (let j = 0; j < this.scale; j++) {
+                array[i + j] = r;
+                array[i + j * 1] = g;
+                array[i + j * 2] = b;
+                array[i + j * 3] = a;
+            }
+        }
+        return array;
     }
 
-    getWindowArray() {
-        ctx.rect(0,0,160, 144),
-        ctx.fillStyle= "rgba(0, 0, 0, 0))";
-        ctx.fill();
+    render() {
+        if (!this.isLCDEnabled()) return;
+
+        this.context.clearRect(0, 0, this.width * this.scale, this.height * this.scale);
+        this.sprites_array = this.getFilledDisplay(0, 0, 0, 0);
+
+        let doSprites = this.memory.isBitSet('LCDC', 1);
+        if (doSprites) renderSprites();
+
+        //Continue render method.
+
+        let windowImage = new ImageData(this.window_array, 160 * this.scale);
+        let bgImage = new ImageData(this.bg_array, 160 * this.scale);
+        let spritesImage = new ImageData(this.sprites_array, 160 * this.scale);
+
+        this.context.putImageData(bgImage, 0, 0);
+        this.context.putImageData(windowImage, 0, 0);
+        this.context.putImageData(spritesImage, 0, 0);
     }
 
-    getSpritesArray() {
-        ctx.rect(0,0,160, 144),
-        ctx.fillStyle= "rgba(0, 0, 0, 0)";
-        ctx.fill();
+    isLCDEnabled() {
+        return this.memory.isBitSet('LCDC', 7);
+    }
+
+    renderSprites() {
+        //Add rendering of sprites.
     }
 }
 export default Display;
